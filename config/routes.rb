6503586b03b0f0f3efbcee0eb6daf65610ec933d
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-06-05 18:01:34 +0100

    added cards on the index added sticky map and added hover effect on the cards

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "restaurants#index"
-  # resources :restaurants, only: :index
+  resources :restaurants, only: :show
 end
